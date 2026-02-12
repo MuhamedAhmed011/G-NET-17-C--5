3b0e4f0a953d59f841f3a2a50e67aeea5f019fd3
MuhamedAhmed011 <aklmuhamedahmed@gmail.com>
2026-02-12 09:36:44 +0200

    Part01

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,9 +2,44 @@
 {
     internal class Program
     {
+
+        #region Enum
+        //enum DayOfWeek
+        //{
+        //    Saturday = 1,
+        //    Sunday,
+        //    Monday,
+        //    Tuesday,
+        //    Wednesday,
+        //    Thursday,
+        //    Friday
+        //} 
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01
+            //Console.Write("Enter a day number (1-7): ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //// Convert number to enum
+            //DayOfWeek day = (DayOfWeek)number;
+
+            //Console.WriteLine("Day: " + day);
+
+            //// Check Workday or Weekend
+            //switch (day)
+            //{
+            //    case DayOfWeek.Saturday:
+            //    case DayOfWeek.Sunday:
+            //        Console.WriteLine("It's the Weekend");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("It's a Workday");
+            //        break;
+            //}
+            //Console.ReadLine(); 
+            #endregion
         }
     }
 }
